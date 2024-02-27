3dc2f6b1ddd56a6d553d9358aab69213702e9d2b
Habiba-Hashem25 <99210559+Habiba-Hashem25@users.noreply.github.com>
2024-02-27 22:34:27 +0200

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [x, setx]= useState([])
+  const inputRef = useRef()
+  const add = () =>
+  {
+    const value = inputRef.current.value
+    const newData ={completed : false, value}
+    // console.log (value)
+    setx([...x , newData])
+    inputRef.current.value =""
+  }
+
+const itemDone = (index) =>  {
+  const newx =[...x]
+  // const newx =x.slice()
+  x[index].completed = !x[index].completed
+  setx(newx)
+}
+const toDelete = (index) =>  {
+  const newx =[...x]
+  newx.splice(index, 1)
+  setx(newx)  
+}
+// console.log(x)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>To Do List</h2>
+     <ul>
+      {
+        // x.map((item => {
+        //   return <li onClick={ () => itemDone(index)}>{item.value}</li>
+        // }))
+        x.map(({value, completed}, index) => {
+          return <div className='div1'>
+            <li className={completed? "diffstyle" : ""} onClick={ () => itemDone(index)}>{value}</li>
+          <span onClick={ () => toDelete (index)}>X</span>
+          </div>
+        })
+      }
+     </ul>
+     <input ref={inputRef} placeholder='Enter New Task' required />
+     <button onClick={add}>Add</button>
     </div>
   );
 }
